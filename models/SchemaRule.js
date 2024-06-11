cf78390c18ace8862b749f05a9c98e148429be15
noman-092 <numanansari078@gmail.com>
2024-06-11 23:09:41 +0530

    .

--- models/SchemaRule.js
+++ models/SchemaRule.js
@@ -1,20 +1,47 @@
 const mongoose = require('mongoose');
 
 const SchemaBook = new mongoose.Schema({
-    Poster: String,
-    Name: {
-       type:String,
-       minLength:4,
-       trim:true,
+    
+   Name: {
+    type:String,
+    trim:true,
+    require:true,
         
     },
-    Author: String,
-    Isbn: String,
-    Price: Number,
-    description:String,
+   Title:{
+    type:String,
+    require:true,
+    trim:true,
+   },
+   Price: {
+    type:String,
+    trim:true,
+    require:true,
+     
+ },
+ Address: {
+    type:String,
+    trim:true,
+    require:true,
+     
+ },
+ Contact: {
+    type:String,
+    require:true,
+    maxLength:10,
+    trim:true,
+     
+ },
+ Poster:{
+    type:String,
+    require:true,
+ },
+ description:{
+    type:String,
+ }
 
 });
 
-const BookCollection = mongoose.model("kotwe",SchemaBook);
+const BookCollection = mongoose.model("detail",SchemaBook);
 
 module.exports = BookCollection;
